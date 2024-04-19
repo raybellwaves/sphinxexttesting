0e80830a27cb48e4d0c07e1778df570c71d4bd6f
Ray Bell <ray.bell@dtn.com>
2024-04-19 11:22:18 -0400

    commit

--- sphinxexttesting.py
+++ sphinxexttesting.py
@@ -1,12 +1,15 @@
-def get_random_ingredients(kind=None):
+def get_random_ingredients(kind: list[str] = None) -> list[str]:
     """
     Return a list of random ingredients as strings.
 
-    :param kind: Optional "kind" of ingredients.
-    :type kind: list[str] or None
-    :raise lumache.InvalidKindError: If the kind is invalid.
-    :return: The ingredients list.
-    :rtype: list[str]
+    Parameters
+    ----------
+    kind : list[str]
+        Optional “kind” of ingredients.
+
+    Returns
+    -------
+    The ingredients list: list[str]
 
     .. todo:: This is in the docstring
     """
